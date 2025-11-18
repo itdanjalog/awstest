3067a1b8522be0dacaeff69d6b6367d903166f85
itdanjalog <kgs2072@naver.com>
2025-11-18 13:24:25 +0900

    STEP1

--- src/main/java/web/model/repository/TestEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package web.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import web.model.entity.TestEntity;
-
-@Repository
-public interface TestEntityRepository extends JpaRepository<TestEntity , Integer > {
-}
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
--- 샘플
-INSERT INTO test ( content ) VALUES ('test1'),('test2'),('test2');
