7ded124e02b72c9a3deaf44baee881f7e306d697
itdanjalog <kgs2072@naver.com>
2025-11-19 16:27:19 +0900

    STEP2

--- src/main/java/web/controller/RestTestController.java
+++ src/main/java/web/controller/RestTestController.java
@@ -16,12 +16,14 @@ public class RestTestController {
 
     private final RestTestService restTestService;
 
+    // http://spring-env.eba-pynxe7sz.ap-northeast-2.elasticbeanstalk.com/test/rest
     // 1. 일반 REST
     @GetMapping("/rest") // [GET] http://localhost:8080/test/rest
     public String test1(){
         return "Hello Rest";
     }
 
+    //
     // 2. DB ( RDS ) REST
     @PostMapping("/rds") // [POST] http://localhost:8080/test/rds?content=클라우드테스트
     public List<TestEntity> test2(@RequestParam String content){
