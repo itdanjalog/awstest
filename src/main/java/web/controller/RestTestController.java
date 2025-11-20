abd6ed293288424d917b92fcbf3fa0c2c2c7f6a9
itdanjalog <itdanja@kakao.com>
2025-11-21 08:08:33 +0900

    -

--- src/main/java/web/controller/RestTestController.java
+++ src/main/java/web/controller/RestTestController.java
@@ -10,32 +10,32 @@ import web.service.RestTestService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class RestTestController {
 
     private final RestTestService restTestService;
 
     // 1. 일반 REST
-    @GetMapping("/rest") // [GET] http://localhost:8080/test/rest
+    @GetMapping("/rest") // [GET] http://localhost:8080/api/rest
     public String test1(){
         return "Hello Rest";
     }
 
     // 2. DB ( RDS ) REST
-    @PostMapping("/rds") // [POST] http://localhost:8080/test/rds?content=클라우드테스트
+    @PostMapping("/rds") // [POST] http://localhost:8080/api/rds?content=클라우드테스트
     public List<TestEntity> test2(@RequestParam String content){
         return restTestService.test2( content );
     }
 
     // 3. REDIS( 캐싱 ) REST
-    @PostMapping("/cache") // [POST] http://localhost:8080/test/cache?content=캐싱
+    @PostMapping("/cache") // [POST] http://localhost:8080/api/cache?content=캐싱
     public List<String> test3( @RequestParam String content ){
         return restTestService.test3( content );
     }
 
     // 4. S3 (파일 업로드/삭제 ) REST
-    @PostMapping("/s3")     // [POST] http://localhost:8080/test/s3 , multipart/form-data , file
+    @PostMapping("/s3")     // [POST] http://localhost:8080/api/s3 , multipart/form-data , file
     public String test4(  MultipartFile file ){
         return restTestService.test4( file );
     }
