0316d03cb3c3736faaa5d46bae097594abdb0107
itdanjalog <kgs2072@naver.com>
2025-11-24 10:24:29 +0900

    STEP6 테스트

--- src/main/reactapp/src/Home.jsx
+++ src/main/reactapp/src/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
     <div>
       <h2>Home Page</h2>
       <p>간단한 메시지: 리액트 라우터 홈 페이지 입니다.</p>
-      <p> STEP6 : CI/CD 테스트 </p>
+      <p>STEP6 브랜치 코드를 PUSH 했을때 자동으로 배포 되는 테스트 </p>
     </div>
   );
 }
